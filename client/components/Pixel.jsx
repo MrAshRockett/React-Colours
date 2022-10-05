60c250f2add92793be59e073e08f49a6c71e5dbb
Ash Rockett <ashrockett19@gmail.com>
2022-10-05 20:56:42 +1300

    Added more Pixel components. Added initial styling.

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Pixel() {
-  return <div></div>
+  return <div style = {{
+    width: '50px',
+    height: '50px',
+    backgroundColor: '#F39C12',
+  }}></div>
 }
 
-export default 'Pixel'
+export default Pixel

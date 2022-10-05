df0c578cd77c334d122145130671779a27e12b1b
Ash Rockett <ashrockett19@gmail.com>
2022-10-05 21:44:55 +1300

    Colour randomiser added into setStyle. State changes when mouse moves.

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const randomColour = () => 
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 function Pixel() {
-  return <div style = {{
-    width: '50px',
-    height: '50px',
-    backgroundColor: '#F39C12',
-  }}></div>
+  const [style, setStyle] = useState({
+    width: '25px',
+    height: '25px',
+    backgroundColor: '#fff'
+  }) 
+
+  const handlePixel = () => {
+    setStyle ({
+      width: '25px',
+      height: '25px',
+      backgroundColor: randomColour()
+    })
+  }
+
+   return <div style={style} onMouseOver={handlePixel}></div >
 }
 
 export default Pixel
